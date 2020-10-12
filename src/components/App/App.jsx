7b7f15e964b54dbdab5efd33dffc98e897a4342e
Jakob <jakob_gauffin@hotmail.com>
2020-10-12 15:31:09 +0200

    First commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TempMessage from "../TempMessage/TempMessage";
+
+const App = () => {
+  return (
+    <div>
+      <TempMessage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TempMessage/TempMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./TempMessage.scss";
+
+const TempMessage = () => {
+  return (
+    <div className="TempMessage">
+      <h1 className="TempMessage__Title">Massa bra text om Hooli</h1>
+    </div>
+  )
+}
+
+export default TempMessage
