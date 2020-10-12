f9d28bab3b323dcbd4556cd7c876f9a940eb219b
Jakob <jakob_gauffin@hotmail.com>
2020-10-12 16:52:24 +0200

    Add testbranch and square

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TempMessage from "../TempMessage/TempMessage";
+import Square from "../Square/Square";
 
 const App = () => {
   return (
     <div>
       <TempMessage />
+      <Square />
     </div>
   )
 }
---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./Square.scss";
+
+const Square = () => {
+  return (
+    <div className="Square">
+      sdasdas
+    </div>
+  )
+}
+
+export default Square
